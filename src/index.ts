b0a7d25b7187677cc2ef577df59e5b17b37c23b3
Zviad Metreveli <thezviad@gmail.com>
2020-12-06 18:12:24 -0500

    [sdk] export all useful sdk parts in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 export { SavingsKit } from "./savingskit"
+export { newVoterV1, VoterV1 } from "./voterv1"
+
+export { SavingsCELO } from "../types/web3-v1-contracts/SavingsCELO"
+export { SavingsCELOVoterV1 } from "../types/web3-v1-contracts/SavingsCELOVoterV1"
+export { SavingsCELOVGroup } from "../types/web3-v1-contracts/SavingsCELOVGroup"
