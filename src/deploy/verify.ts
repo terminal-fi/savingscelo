7e0516b9ab58d1aaffc5ab6b49300e61d554b406
Zviad Metreveli <thezviad@gmail.com>
2020-12-28 11:05:55 -0500

    clean out verify.ts script

--- src/deploy/verify.ts
+++ src/deploy/verify.ts
@@ -1,10 +1,15 @@
 import fs from "fs"
-import path from "path"
 
 import axios from "axios"
 
 async function main() {
-	const flattened = fs.readFileSync(path.join(__dirname, "..", "..", "flat.sol")).toString()
+	// TODO(zviad): verifying contracts still doesn't work on blockscout.
+	// This is just a test script until then. Once blockscout API is fixed it can
+	// support verifying for any of the networks.
+
+	// TODO(zviad): run:
+	// > npx truffle-flattener contracts/SavingsCELO.sol > /tmp/SavingsCELO.flat.sol
+	const flattened = fs.readFileSync("/tmp/SavingsCELO.flat.sol").toString()
 	try {
 		// const resp = await axios.get(
 		// 	"https://baklava-blockscout.celo-testnet.org/api?module=contract&action=listcontracts")
