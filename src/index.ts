ad95a087f2a35397253a3aecb0dc0081157d8540
Zviad Metreveli <thezviad@gmail.com>
2021-02-14 10:06:11 -0500

    re-export contract addresses for easy importing

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ export { newVoterV1, VoterV1 } from "./voterv1"
 export { SavingsCelo as SavingsCELO } from "../types/web3-v1-contracts/SavingsCELO"
 export { SavingsCeloVoterV1 as SavingsCELOVoterV1 } from "../types/web3-v1-contracts/SavingsCELOVoterV1"
 export { SavingsCelovGroup as SavingsCELOVGroup } from "../types/web3-v1-contracts/SavingsCELOVGroup"
+
+export { address as SavingsCELOAddressAlfajores } from "./deploy/alfajores.SavingsCELO.addr.json"
+export { address as SavingsCELOAddressBaklava } from "./deploy/baklava.SavingsCELO.addr.json"
