92b7d76e9b84b3f1c265a565a08a513105957fb0
Zviad Metreveli <thezviad@gmail.com>
2021-04-04 09:06:11 -0400

    remove unused SavingsVG

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export { SavingsKit } from "./savingskit"
 export { newVoterV1, VoterV1 } from "./voterv1"
-export { SavingsVG } from "./savingsvg"
 
 export { SavingsCelo as SavingsCELO } from "../types/web3-v1-contracts/SavingsCELO"
 export { SavingsCeloVoterV1 as SavingsCELOVoterV1 } from "../types/web3-v1-contracts/SavingsCELOVoterV1"
