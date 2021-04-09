a3dbcb1d81a2285443c436d10c44069c2e035a50
Zviad Metreveli <thezviad@gmail.com>
2021-04-09 13:51:46 -0400

    [test] change logfile location

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 trap 'jobs -p | xargs -r kill || true' EXIT
-echo "Starting celo-devchain on port 7545, logs: /tmp/savingscelo.celo-devchain.log ..."
-yarn celo-devchain --port 7545 &> /tmp/savingscelo.celo-devchain.log &
+echo "Starting celo-devchain on port 7545, logs: /tmp/test.celo-devchain.log ..."
+yarn celo-devchain --port 7545 &> /tmp/test.celo-devchain.log &
 while ! nc -z localhost 7545; do
   sleep 0.1 # wait for 1/10 of the second before check again
 done
